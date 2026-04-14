2f5a077c572374679db10ddd05fc706de79e9daa
windesk <mstkmp-github@proton.me>
2026-04-13 18:27:07 -0600

    Animations
    Add movement animations and add grounded detection

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,22 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Windows;
 
 [RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Animator))]
 public class PlayerController : MonoBehaviour
 {
     [Header("Components")]
     private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+    private new Collider2D collider;
+    private Animator animator;
 
     [Header("Properties")]
-    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float walkSpeed = 5f;
     [SerializeField] private float stopVelocityMultiplier = 0.5f;
     [SerializeField] private float jumpForce = 5f;
     [SerializeField] private float jumpDuration = 1f;
@@ -17,7 +24,6 @@ public class PlayerController : MonoBehaviour
     private Vector2 moveDirection;
 
     [Header("States")]
-    [SerializeField] private bool isGrounded = true;
     [SerializeField] private bool isAlive = true;
     private bool IsAlive
     {
@@ -34,21 +40,50 @@ public class PlayerController : MonoBehaviour
         set 
         {
             isJumping = value;
+            animator.SetBool("IsJumping", isJumping);
         }
     }
-    [SerializeField] private bool isMoving = false;
-    private bool IsMoving
+    [SerializeField] private bool isFalling = false;
+    private bool IsFalling
     {
-        get { return isMoving; }
+        get { return isFalling; }
         set
         {
-            isMoving = value;
+            isFalling = value;
+            animator.SetBool("IsFalling", isFalling);
         }
     }
+    [SerializeField] private bool isWalking = false;
+    private bool IsWalking
+    {
+        get { return isWalking; }
+        set
+        {
+            isWalking = value;
+            animator.SetBool("IsWalking", isWalking);
+        }
+    }
+    [SerializeField] private bool isGrounded = false;
+    private bool IsGrounded
+    {
+        get { return isGrounded; }
+        set
+        {
+            isGrounded = value;
+            animator.SetBool("IsGrounded", isGrounded);
+        }
+    }
+
+
+    private Vector2 origin;
+    private RaycastHit2D hit;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+        collider = GetComponent<Collider2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Start()
@@ -56,10 +91,36 @@ public class PlayerController : MonoBehaviour
 
     }
 
-    void Update()
+    private void Update()
     {
-        rb.linearVelocityX = (IsMoving) ? moveDirection.x : rb.linearVelocityX;
-        rb.linearVelocityY = (IsJumping && jumpEndTime >= Time.time) ? jumpForce : rb.linearVelocity.y;
+        LocamotionHandler();
+    }
+
+    private void LocamotionHandler()
+    {
+        // Walking
+        rb.linearVelocityX = (IsWalking) ? moveDirection.x : rb.linearVelocityX;
+
+        // Jumping
+        if (IsJumping && jumpEndTime >= Time.time)
+        {
+            rb.linearVelocityY = jumpForce;
+        }
+        else
+        {
+            IsJumping = false;
+            rb.linearVelocityY = rb.linearVelocity.y;
+        }
+
+        // Falling & Grounded
+        IsFalling = rb.linearVelocity.y < 0 && !IsGrounded;
+        IsGrounded = hit.collider != null;
+    }
+
+    void FixedUpdate()
+    {
+        origin = new Vector2(transform.position.x, transform.position.y - collider.bounds.extents.y - .01f);
+        hit = Physics2D.Raycast(origin, Vector2.down, .01f);
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -68,21 +129,22 @@ public class PlayerController : MonoBehaviour
 
         if (context.performed)
         {
-            IsMoving = true;
+            IsWalking = true;
             Debug.Log("Started moving");
-            moveDirection = input.normalized * moveSpeed;
+            moveDirection = input.normalized * walkSpeed;
+            sprite.flipX = moveDirection.x < 0;
         }
         else if (context.canceled)
         {
-            IsMoving = false;
+            IsWalking = false;
             Debug.Log("Stopped moving");
-            if(isGrounded) { rb.linearVelocityX *= stopVelocityMultiplier; }
+            if(IsGrounded) { rb.linearVelocityX *= stopVelocityMultiplier; }
         }
     }
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        if (context.performed && isGrounded)
+        if (context.performed && IsGrounded)
         {
             IsJumping = true;
             Debug.Log("Started jumping");
